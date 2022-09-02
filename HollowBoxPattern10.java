5a3d0a7f602c851a665e962812a03fae51484692
Antoflomine <68322807+Antoflomine@users.noreply.github.com>
2022-09-02 13:20:35 +0530

    Create HollowBoxPattern10.java

---
+++ HollowBoxPattern10.java
@@ -0,0 +1,32 @@
+
+public class Main
+{
+	public static void main(String[] args) {
+		int n=5;
+		int s=(n*2)-1;
+		int front=0;
+		int end=s-1;
+		int arr[][]=new int[s+1][s+1];
+		while(n!=0){
+		    for(int i=front;i<=end;i++){
+		        for(int j=front;j<=end;j++){
+		            if(i==front || j==front || i==end || j==end){
+		                int k=n%2;
+		                arr[i][j]=k;
+		            }
+		        
+		        }
+		       
+		    }
+		  n--;
+		    front++;
+		    end--;
+		}
+		for(int i=0;i<s;i++){
+		    for(int j=0;j<s;j++){
+		        System.out.print(arr[i][j]);
+		    }
+		    System.out.println();
+		}
+	}
+}
