50a8045cc594608af916df8ca1be73d115113cac
Antoflomine <68322807+Antoflomine@users.noreply.github.com>
2023-12-15 19:39:14 +0530

    Second largest number without java.java

---
+++ Round 2/Second largest number.java
@@ -0,0 +1,23 @@
+
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+	    Scanner sc=new Scanner(System.in);
+	    int n=sc.nextInt();
+	    int[] arr=new int[n];
+	    for(int i=0;i<n;i++){
+	        arr[i]=sc.nextInt();
+	    }
+	    int f=arr[0];
+	    int s=arr[0];
+	    for(int i=0;i<n;i++){
+	        if(f<arr[i]){
+	            s=f;
+	            f=arr[i];
+	            
+	        }
+	    }
+		System.out.println(s);
+	}
+}
