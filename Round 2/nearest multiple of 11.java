6ca04258c529ba7f1b4c903164ace47b271f8827
Antoflomine <68322807+Antoflomine@users.noreply.github.com>
2023-12-18 20:07:55 +0530

    Update nearest multiple of 11.java

--- Round 2/nearest multiple of 11.java
+++ Round 2/nearest multiple of 11.java
@@ -9,7 +9,7 @@ public class Main
 	    Scanner sc=new Scanner(System.in);
 	    int n=sc.nextInt(); //4722 ->4720 4730
 	    
-	   int a=(n/10)*11;
+	   int a=(n/11)*11;
 	   int b=a+11;
 	 
 	   if(n-a<b-n){
